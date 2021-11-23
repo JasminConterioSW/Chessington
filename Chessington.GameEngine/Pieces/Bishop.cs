0a4a05e1b57deb91c2ad0d0bc4c8e080a77d2fdf
Jasmin Conterio <Jasmin.Conterio@softwire.com>
2021-11-23 14:39:44 +0000

    Passing Red-04, but not pretty

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,54 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            const int boardSize = 8;
+            
+            var availableMoves = new List<Square>();
+            var currentPosition = board.FindPiece(this);
+
+            var i = currentPosition.Row;
+            var j = currentPosition.Col;
+
+            while (i < boardSize && j < boardSize)
+            {
+                availableMoves.Add(Square.At(i,j));
+                i += 1;
+                j += 1;
+            }
+            
+            i = currentPosition.Row;
+            j = currentPosition.Col;
+            
+            while (i >= 0 && j >= 0)
+            {
+                availableMoves.Add(Square.At(i,j));
+                i -= 1;
+                j -= 1;
+            }
+            
+            i = currentPosition.Row;
+            j = currentPosition.Col;
+            
+            while (i >= 0 && j < boardSize)
+            {
+                availableMoves.Add(Square.At(i,j));
+                i -= 1;
+                j += 1;
+            }
+            
+            i = currentPosition.Row;
+            j = currentPosition.Col;
+            
+            while (i <boardSize  && j>=0)
+            {
+                availableMoves.Add(Square.At(i,j));
+                i += 1;
+                j -= 1;
+            }
+
+            availableMoves.RemoveAll(m => m == currentPosition);
+            
+            return availableMoves;
         }
     }
 }
